9329b71e41689c6e7c7d431ff8f125692eecc0cf
abrighto <Allan.Brighton@t-online.de>
2021-12-08 22:31:21 +0100

    added formula

--- Formula/tcs-vslice.rb
+++ Formula/tcs-vslice.rb
@@ -2,7 +2,7 @@ class TcsVslice < Formula
   desc "Shared libraries for TMT TCS assembly"
   homepage "https://github.com/tmtsoftware/tcs-vslice-0.4"
   url "https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.3/tcs-vslice-dylibs.zip"
-  sha256 "dde98e3cbce3a5cc62585a9e34c0ec652d6310c5043e4d2b78c7780b446d892b"
+    sha256 "7af66df9e523dc20343eef645cb3594216be88c36f3a6820253788f52d70d006"
   license "GPL-2.0-or-later"
 
   depends_on "libcbor"
