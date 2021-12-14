a06f77561c7393353161dafa1310b0bc4fc1613d
abrighto <Allan.Brighton@t-online.de>
2021-12-14 15:51:32 +0100

    Updated for v0.5

--- Formula/tcs-vslice.rb
+++ Formula/tcs-vslice.rb
@@ -1,8 +1,8 @@
 class TcsVslice < Formula
   desc "Shared libraries for TMT TCS assembly"
   homepage "https://github.com/tmtsoftware/tcs-vslice-0.4"
-  url "https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.4/tcs-vslice-dylibs.zip"
-    sha256 "3bea3b57daf15383d09799b665e05982420bf214226b4b50c4860a077ece02a9"
+  url "https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.5/tcs-vslice-dylibs.zip"
+    sha256 "5f543a03574fb4c9d101c1973a8961857101fbcb49aa434efe1de752cf265e48"
   license "GPL-2.0-or-later"
 
   depends_on "libcbor"
