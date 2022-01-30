3758a8395fd89ee8cd61ee40a449dc7b4252a9dd
abrighto <Allan.Brighton@t-online.de>
2022-01-31 00:48:18 +0100

    Updated for v0.6

--- Formula/tcs-vslice.rb
+++ Formula/tcs-vslice.rb
@@ -1,8 +1,8 @@
 class TcsVslice < Formula
   desc "Shared libraries for TMT TCS assembly"
   homepage "https://github.com/tmtsoftware/tcs-vslice-0.4"
-  url "https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.5/tcs-vslice-dylibs.zip"
-    sha256 "5f543a03574fb4c9d101c1973a8961857101fbcb49aa434efe1de752cf265e48"
+  url "https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.6/tcs-vslice-dylibs.zip"
+    sha256 "8c3c7f3fd2a138c723cdeaf6adcd0c7c42d55a310866ea602aab9cf88aa6b57d"
   license "GPL-2.0-or-later"
 
   depends_on "libcbor"
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Or `brew tap abrighton/tcs-vslice` and then `brew install <formula>`.
 ## Documentation
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+Note: Update tcs-vslice.rb with the new path and sha: `shasum -a 256 tcs-vslice-dylibs.zip`.
+
